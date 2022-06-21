7d4f639f427c3f3a6cb969783a60ebfb3fce4b1d
Piyushkr44 <senditto.piyush@gmail.com>
2022-06-21 11:07:22 +0000

    added files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user_entity (id,first_name,last_name) values (1,'Piyush','Kumar');
+insert into user_entity (id,first_name,last_name) values (2,'Vinit','Dwivedi');
+insert into user_entity (id,first_name,last_name) values (3,'Shubham','Kumar');
